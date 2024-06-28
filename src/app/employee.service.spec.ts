7abe7a577e4fac7fa5bb3a51040680555b8da6b6
Ahmad <ahmad10javaid@gmail.com>
2024-06-28 18:18:56 +0500

    services

--- src/app/employee.service.spec.ts
+++ src/app/employee.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ResvService } from './resv.service';
+import { EmployeeService } from './employee.service';
 
-describe('ResvService', () => {
-  let service: ResvService;
+describe('EmployeeService', () => {
+  let service: EmployeeService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ResvService);
+    service = TestBed.inject(EmployeeService);
   });
 
   it('should be created', () => {
